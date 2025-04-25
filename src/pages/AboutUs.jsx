8a65db51aab5141d03847a535e4afdbb82f3469e
Abhigaikwad326 <abhijit.gaikwad326@gmail.com>
2025-04-25 14:37:43 +0530

    contact

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
 import Nav from '../components/Nav'
-
+import "./style2.css"
+import abt from '../assets/images/abouti.png';
+import b1 from '../assets/images/rocket-icon.png';
+import b2 from '../assets/images/lock icon.png';
+import b3 from '../assets/images/bulb-icon.png';
 const AboutUs = () => {
   return (
-    <div>
+    <div className="about">
+    <div className="main">
       <Nav/>
-      AboutUs</div>
+    <h1>Empowering Your Investment Decisions with <br/> Clarity, Confidence & Compliance</h1>
+    <div className="content">
+      <div className="column">
+    <div className="text1">At Research Analyst Services, we provide data- <br/>driven market strategies and equity insights to <br />empower investors.</div>
+    <div className="text2">We simplify investing by delivering clear, actionable research <br /> on market trends, economic signals, and company <br /> fundamentals — helping clients make confident decisions <br />and build goal-focused portfolios.</div>
+    </div>
+  <div className="image"> < img src={abt}alt=""></img> </div>
+      </div>
+      <button>Explore Services</button>
+      </div>
+      <div className="sec">
+        <h2>Our Misson</h2>
+        <p>To provide transparent, research-first investment insights that help  <br/>individuals and professionals grow wealth with clarity and confidence.</p>
+      <div className="boxs">
+      <div className="box1">
+      <img src={b1} alt=""/>
+      </div>
+      <div className="box1">
+      <img src={b2} alt=""/>
+      </div>
+      <div className="box1">
+      <img src={b3} alt=""/>
+        </div>
+        </div>
+        <div className="boxtext">
+        <div className="boxt">
+          <h3>Accessibility</h3>
+          <p>We aim to make trading seamless and approachable for everyone—whether you're a beginner or a seasoned investor. Our platform is designed to simplify market access and foster confident decision-making.</p>
+        </div>
+        <div className="boxt">
+          <h3>Trust</h3>
+          <p>Your trust is our top priority. From data security to transparent operations, we are committed to building a reliable environment where users feel secure and empowered to trade.</p>
+        </div>
+        <div className="boxt">
+          <h3>Innovation</h3>
+          <p>In a fast-moving financial landscape, innovation is our edge. We constantly evolve to offer cutting-edge tools, intuitive interfaces, and powerful insights—putting the future of investing at your fingertips.</p>
+        </div>
+        
+        </div>
+        </div>
+        <div className="vision"></div>
+        </div>    
   )
 }
 
-export default AboutUs
+export default AboutUs     
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,11 +1,86 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import icon1 from '../assets/images/port.png';
+import icon2 from '../assets/images/bulb2.png';
+import icon3 from '../assets/images/bell.png';
+
 
 const ContactUs = () => {
   return (
-    <div>
+    <div className="contact">
       <Nav/>
-      ContactUs</div>
+      <div className="letConnet">
+        <div className="head1">
+        <h2>Let’s Connect</h2>
+        <p>Have a question, need expert guidance, or want to explore our services in more depth?</p>
+        </div>
+      <div className="let1">
+        <div className="con">
+        <div className="one"><img src={icon1} alt="" /> </div>
+         <div className="two"><p>Optimize your portfolio using expert-crafted <br /> strategies backed by historical data,  <br />predictive analytics, and proven financial <br /> models.</p></div> 
+        </div>
+        <div className="con">
+
+          <div className="one"><img src={icon2} alt="" /> </div>
+          <div className="two"><p>Trade with confidence. Get daily or weekly <br />trading ideas based on technical indicators <br />and market sentiment — delivered in a way <br /> that’s easy to act on.</p></div>
+        </div> 
+        <div className="con">
+        <div className="one"><img src={icon3} alt="" /> </div>
+        <div className="two"><p>Never miss an opportunity. Stay updated <br /> with timely alerts for stock movements,<br /> trend reversals, and critical news that <br />impacts your investments.</p></div>
+        </div>
+      </div>
+  
+{/* form container  */}
+
+    <div className="form-container">
+      <form className="form">
+        <div className="form-row">
+          <div className="form-group">
+            <label>First name <span>*</span></label>
+            <input type="text" placeholder="Name" />
+          </div>
+          <div className="form-group">
+            <label>Last name <span>*</span></label>
+            <input type="text" placeholder="Surname" />
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label>Email id <span>*</span></label>
+            <input type="email" placeholder="name@example.com" />
+          </div>
+          <div className="form-group">
+            <label>Phone no. <span>*</span></label>
+            <input type="tel" placeholder="+91 **********" />
+          </div>
+        </div>
+
+        <div className="form-group full">
+          <label>City</label>
+          <select>
+            <option>Mumbai</option>
+            <option>Thane</option>
+            <option>New-Mumbai</option>
+          </select>
+        </div>
+
+        <div className="form-group full">
+          <label>How can we help?</label>
+          <textarea placeholder="Your message..."></textarea>
+        </div>
+
+        <p className="policy-text">
+          By submitting this, I confirm that I have read and understood the <a href="#">Privacy Policy</a>.
+        </p>
+
+        <div className="submit-btn-wrapper">
+          <button type="submit" className="submit-btn">Submit</button>
+        </div>
+      </form>
+    </div>
+      </div>
+      </div>
   )
 }
 
